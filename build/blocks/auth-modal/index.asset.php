6f77e7375cfc6e79b5074a6df51e161a35b9f5e2
VictorScoolExper <victorscool477@outlook.com>
2024-02-12 17:09:31 -0600

    fixed margin left tab problem css

--- build/blocks/auth-modal/index.asset.php
+++ build/blocks/auth-modal/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '1e832e424c737d97601d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '13a7f2c65efb508c28e3');
